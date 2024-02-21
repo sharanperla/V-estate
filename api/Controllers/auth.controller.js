47160d29443e2dadb55251b69c35e0be0762849f
sharan kumar <sharanperla1234@gmail.com>
2024-02-21 22:52:16 +0530

    completed signup ui

--- api/Controllers/auth.controller.js
+++ api/Controllers/auth.controller.js
@@ -1,7 +1,7 @@
 import User from "../Models/user.models.js";
 import bcryptjs from "bcryptjs";
 
-export const signup=async (req,res)=>{
+export const signup=async (req,res,next)=>{
 const {username,email,password}=req.body;
 const hashPassword=bcryptjs.hashSync(password,10);
 const newUser=new User({username,email,password:hashPassword}); 
@@ -10,6 +10,6 @@ try {
     await newUser.save()
     res.status(201).json('created successfully');  
 } catch (error) {
-    res.status(500).json(error.message);
+   next(error)
 }
 }
